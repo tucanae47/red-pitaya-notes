775759a78a90b059991294e36ec625fc2739fc08
tucanae47 <cmsvalenzuela@gmail.com>
2023-05-31 11:00:31 +0200

    Add BRAM axi example only

---
+++ projects/bram_test/block_design.tcl
@@ -0,0 +1,130 @@
+# Create clk_wiz
+cell xilinx.com:ip:clk_wiz pll_0 {
+  PRIMITIVE PLL
+  PRIM_IN_FREQ.VALUE_SRC USER
+  PRIM_IN_FREQ 125.0
+  PRIM_SOURCE Differential_clock_capable_pin
+  CLKOUT1_USED true
+  CLKOUT1_REQUESTED_OUT_FREQ 125.0
+  USE_RESET false
+} {
+  clk_in1_p adc_clk_p_i
+  clk_in1_n adc_clk_n_i
+}
+
+# Create processing_system7
+cell xilinx.com:ip:processing_system7 ps_0 {
+  PCW_IMPORT_BOARD_PRESET cfg/red_pitaya.xml
+} {
+  M_AXI_GP0_ACLK pll_0/clk_out1
+}
+
+# Create all required interconnections
+apply_bd_automation -rule xilinx.com:bd_rule:processing_system7 -config {
+  make_external {FIXED_IO, DDR}
+  Master Disable
+  Slave Disable
+} [get_bd_cells ps_0]
+
+# Create xlconstant
+cell xilinx.com:ip:xlconstant const_0
+
+# Create proc_sys_reset
+cell xilinx.com:ip:proc_sys_reset rst_0 {} {
+  ext_reset_in const_0/dout
+}
+
+# Create axi_cfg_register
+cell pavel-demin:user:axi_cfg_register cfg_0 {
+  CFG_DATA_WIDTH 128
+  AXI_ADDR_WIDTH 4
+  AXI_DATA_WIDTH 32
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_0 {
+  DIN_WIDTH 128 DIN_FROM 0 DIN_TO 0
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_1 {
+  DIN_WIDTH 128 DIN_FROM 47 DIN_TO 32
+} {
+  din cfg_0/cfg_data
+}
+
+# Create port_slicer
+cell pavel-demin:user:port_slicer slice_2 {
+  DIN_WIDTH 128 DIN_FROM 127 DIN_TO 48
+} {
+  din cfg_0/cfg_data
+}
+
+# Create blk_mem_gen
+cell xilinx.com:ip:blk_mem_gen bram_0 {
+  MEMORY_TYPE True_Dual_Port_RAM
+  USE_BRAM_BLOCK Stand_Alone
+  USE_BYTE_WRITE_ENABLE true
+  BYTE_SIZE 8
+  WRITE_WIDTH_A 32
+  WRITE_DEPTH_A 16384
+  WRITE_WIDTH_B 64
+  ENABLE_A Always_Enabled
+  ENABLE_B Always_Enabled
+  REGISTER_PORTB_OUTPUT_OF_MEMORY_PRIMITIVES false
+}
+
+# Create axi_bram_writer
+cell pavel-demin:user:axi_bram_writer writer_0 {
+  AXI_DATA_WIDTH 32
+  AXI_ADDR_WIDTH 32
+  BRAM_DATA_WIDTH 32
+  BRAM_ADDR_WIDTH 14
+} {
+  BRAM_PORTA bram_0/BRAM_PORTA
+}
+
+# Create axis_bram_reader
+cell pavel-demin:user:axis_bram_reader reader_0 {
+  AXIS_TDATA_WIDTH 64
+  BRAM_DATA_WIDTH 64
+  BRAM_ADDR_WIDTH 13
+  CONTINUOUS FALSE
+} {
+  BRAM_PORTA bram_0/BRAM_PORTB
+  cfg_data slice_1/dout
+  aclk pll_0/clk_out1
+  aresetn slice_0/dout
+}
+
+
+
+# Create axis_data_fifo
+cell xilinx.com:ip:axis_data_fifo fifo_0 {
+  TDATA_NUM_BYTES.VALUE_SRC USER
+  TDATA_NUM_BYTES 4
+  FIFO_DEPTH 16384
+} {
+  S_AXIS reader_0/M_AXIS
+  s_axis_tvalid const_0/dout
+  s_axis_aclk pll_0/clk_out1
+  s_axis_aresetn slice_0/dout
+}
+
+# Create axi_axis_reader
+cell pavel-demin:user:axi_axis_reader reader_1 {
+  AXI_DATA_WIDTH 32
+} {
+  S_AXIS fifo_0/M_AXIS
+  aclk pll_0/clk_out1
+  aresetn rst_0/peripheral_aresetn
+}
+
+addr 0x40001000 4K cfg_0/S_AXI /ps_0/M_AXI_GP0
+
+addr 0x40010000 64K reader_1/S_AXI /ps_0/M_AXI_GP0
+
+addr 0x40030000 64K writer_0/S_AXI /ps_0/M_AXI_GP0
+
---
+++ projects/bram_test/test.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <math.h>
+#include <sys/mman.h>
+
+int main(int argc, char *argv[])
+{
+  int fd, i;
+  uint16_t value, model, cntr, fall, rise, scale;
+  int64_t y[3];
+  volatile void *cfg;
+  volatile uint32_t *fifo, *bram;
+
+  if((fd = open("/dev/mem", O_RDWR)) < 0)
+  {
+    fprintf(stderr, "Cannot open /dev/mem.\n");
+    return EXIT_FAILURE;
+  }
+
+  cfg = mmap(NULL, sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40001000);
+  fifo = mmap(NULL, 16 * sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40010000);
+  bram = mmap(NULL, 16 * sysconf(_SC_PAGESIZE), PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0x40030000);
+
+  *(uint8_t *)(cfg + 0) &= ~1;
+
+  fall = (uint16_t)floor(expf(-logf(2.0) / 125.0 / 100) * 65536.0 + 0.5);
+  rise = (uint16_t)floor(expf(-logf(2.0) / 125.0 / 100 * 1.0e3) * 65536.0 + 0.5);
+
+  cntr = 0;
+  y[0] = 4095 << 9;
+  y[1] = 0;
+  y[2] = 0;
+  while(y[2] <= y[1])
+  {
+    ++cntr;
+    y[2] = y[1];
+    y[1] = y[0] + y[1] * rise / 65536;
+    y[0] = y[0] * fall / 65536;
+  }
+  scale = (uint16_t)(4095 * 65535 / (y[2] >> 9));
+
+  for(i = 0; i < 1024; ++i)
+  {
+    bram[i] = 0;
+  }
+
+  /* set pulse height */
+  bram[6] = 4095;
+  /* set size */
+  *(uint16_t *)(cfg + 4) = 1024;
+  /* set scale factor */
+  *(uint16_t *)(cfg + 6) = scale;
+  /* set fall time */
+  *(uint16_t *)(cfg + 8) = fall;
+  /* set rise time */
+  *(uint16_t *)(cfg + 10) = rise;
+  /* set limits */
+  *(int16_t *)(cfg + 12) = -8192;
+  *(int16_t *)(cfg + 14) = 8191;
+
+  *(uint8_t *)(cfg + 0) |= 1;
+
+  sleep(1);
+
+  y[0] = 4095 << 9;
+  y[1] = 0;
+  y[2] = 0;
+  y[0] *= scale;
+  for(i = 0; i < cntr + 10; ++i)
+  {
+    value = *fifo;
+    model = y[2] >> 25;
+    printf("%d\t%d\t%d\n", value, model, value - model);
+    y[2] = y[1];
+    y[1] = y[0] + y[1] * rise / 65536;
+    y[0] = y[0] * fall / 65536;
+  }
+
+  return EXIT_SUCCESS;
+}
